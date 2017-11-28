1d0f06dc06c06926ca9ff4af31d989d1baf298a5
Nhi Dang <dangnhi169@gmail.com>
2017-11-28 16:23:19 -0600

    add more http call to login

--- app/src/main/java/org/writing/jumpstart/jumpstart2017/LoginActivity.java
+++ app/src/main/java/org/writing/jumpstart/jumpstart2017/LoginActivity.java
@@ -1,14 +1,30 @@
 package org.writing.jumpstart.jumpstart2017;
 
+import android.util.Log;
+
 import android.app.ProgressDialog;
+import android.app.Activity;
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.InjectView;
 
@@ -19,13 +35,16 @@ public class LoginActivity extends Activity {
     ProgressDialog progressDialog;
 
     @InjectView(R.id.email) EditText _emailText;
-    @InjectView(R.id.pass) EditText _lastNameText;
+    @InjectView(R.id.pass) EditText _passText;
     @InjectView(R.id.login) TextView _logInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
 
         Button next = (Button) findViewById(R.id.register);
         next.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +53,81 @@ public class LoginActivity extends Activity {
                 startActivity(reg);
             }
         });
+
+        _logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser(_emailText.getText().toString(),
+                        _passText.getText().toString());
+            }
+        });
     }
+
+    private void loginUser( final String email, final String password) {
+        // Tag used to cancel the request
+        String cancel_req_tag = "login";
+        progressDialog.setMessage("Logging you in...");
+        showDialog();
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                URL_FOR_LOGIN, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    boolean error = jObj.getBoolean("error");
+
+                    if (!error) {
+                        String user = jObj.getJSONObject("user").getString("name");
+                        // Launch User activity
+                        Intent intent = new Intent(
+                                LoginActivity.this,
+                                UserActivity.class);
+                        intent.putExtra("username", user);
+                        startActivity(intent);
+                        finish();
+                    } else {
+
+                        String errorMsg = jObj.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                // Posting params to login url
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        // Adding request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq,cancel_req_tag);
+    }
+
+    private void showDialog() {
+        if (!progressDialog.isShowing())
+            progressDialog.show();
+    }
+    private void hideDialog() {
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
+    }
+}
 }

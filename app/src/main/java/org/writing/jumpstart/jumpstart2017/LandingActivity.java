9db527e3052607b1e20152eb91edec0e19e93bda
argnsoccer <amgras@smu.edu>
2018-01-26 10:48:06 -0600

    landing now connects and is homepage

--- app/src/main/java/org/writing/jumpstart/jumpstart2017/LandingActivity.java
+++ app/src/main/java/org/writing/jumpstart/jumpstart2017/LandingActivity.java
@@ -1,7 +1,32 @@
 package org.writing.jumpstart.jumpstart2017;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landingpage);
+
+        Button next = (Button) findViewById(R.id.SignUpButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent login = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(login);
+            }
+        });
+
+        Button trynow = (Button) findViewById(R.id.TryNowButton);
+        trynow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent trying = new Intent(view.getContext(), ProjectListActivity.class);
+                startActivity(trying);
+            }
+        });
+    }
 }
